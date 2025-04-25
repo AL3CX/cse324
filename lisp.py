0aebcf04e65c1de9f3cd8ed961840aabcd274d15
aneri <amecejfuc3@gmail.com>
2025-04-24 18:20:55 -0600

    Started lisp interpreter project

---
+++ lisp.py
@@ -0,0 +1,8 @@
+# Needed imports
+import math
+import operator as op
+
+#
+
+# Welcome message
+print("Welcome to the fancy new Prompt LISP INTERPRETER, type in LISP commands! >")
---
+++ parse.py
@@ -0,0 +1,5 @@
+# Type Definitions
+SYMBOL = str
+NUMBER = (int, float)
+LIST = list
+ATOM = (SYMBOL, NUMBER)
